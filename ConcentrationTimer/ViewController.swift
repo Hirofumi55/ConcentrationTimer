fd357f9b3049f85b8021edb8f52796594830fb8b
Hirofumi <sasa1235711@gmail.com>
2017-07-04 00:24:05 +0900

    method

--- ConcentrationTimer/ViewController.swift
+++ ConcentrationTimer/ViewController.swift
@@ -19,16 +19,20 @@ class ViewController: UIViewController {
     //タイマー値のステッパー
     @IBOutlet weak var timeStepper: UIStepper!
     
+    //タイマー値
+    var time : Float = 0.0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
     //ステッパーがタップされた時に呼び出されるメソッド
     @IBAction func StepperClicked(_ sender: UIStepper) {
@@ -40,10 +44,19 @@ class ViewController: UIViewController {
     //Startボタン押下で実行されるメソッド
     @IBAction func startButton(_ sender: Any) {
         
-        
+        //タイマー値がテキストフィールドに格納されている数値を超えるまで繰り返す
+        while Float(timeLabel.text!)! > time {
+            
+            //60秒SLEEPした処理を実行する
+            DispatchQueue.main.asyncAfter(deadline: .now() + 60.0) {
+                //タイマー値のインクリメント
+                self.time = self.time + 1.0
+                
+                //プログレスバーの更新
+                let bar = (Float(timeLabel.text!)! - self.time) / (Float(timeLabel.text!)!)
+                timeBar.progress = Float(bar)
+            }
+        }
     }
     
-
-
 }
-
