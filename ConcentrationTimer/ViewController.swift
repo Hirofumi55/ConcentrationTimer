a70468e23eb1db774f89e0a9df1c00ae429b7cd1
Hirofumi <sasa1235711@gmail.com>
2017-07-03 23:22:14 +0900

    StoryView

--- ConcentrationTimer/ViewController.swift
+++ ConcentrationTimer/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //タイマー値が表示されるラベル
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    //タイマー値のプログレスバー
+    @IBOutlet weak var timeBar: UIProgressView!
+    
+    //タイマー値のステッパー
+    @IBOutlet weak var timeStepper: UIStepper!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +29,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //ステッパーがタップされた時に呼び出されるメソッド
+    @IBAction func StepperClicked(_ sender: UIStepper) {
+        let num = Int (sender.value)
+        timeLabel.text = String(num)
+    }
+    
+    
+    //Startボタン押下で実行されるメソッド
+    @IBAction func startButton(_ sender: Any) {
+        
+        
+    }
+    
 
 
 }
